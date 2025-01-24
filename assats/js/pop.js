915209e85e475b37b9708c1f24bbaf762172b33e
Abhi112004 <abhigajera046@gmail.com>
2025-01-24 13:13:13 +0530

    scrollupset

--- assats/js/pop.js
+++ assats/js/pop.js
@@ -1,7 +1,20 @@
 function pop() {
   document.getElementById("popOuter").style.display = "none";
 }
-function lode(){
-    document.getElementById("popOuter").style.display = "block";
+function lode() {
+  document.getElementById("popOuter").style.display = "block";
+}
+var btn = document.getElementById("top");
 
+function up() {
+  if (document.documentElement.scrollTop > 100) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+}
+
+window.onscroll = up;
+function topbtn() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 }
