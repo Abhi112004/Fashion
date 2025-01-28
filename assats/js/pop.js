b0af5afad490d4dc58da3d445d194f17f58d1eb4
Abhi112004 <abhigajera046@gmail.com>
2025-01-28 12:11:05 +0530

    add js and jq function

--- assats/js/pop.js
+++ assats/js/pop.js
@@ -1,11 +1,16 @@
+
+function disableScroll(e) {
+  e.preventDefault(); 
+}
 function pop() {
   document.getElementById("popOuter").style.display = "none";
+  window.removeEventListener("wheel", disableScroll, { passive: false });
 }
 function lode() {
   document.getElementById("popOuter").style.display = "block";
+  window.addEventListener("wheel", disableScroll, { passive: false });
 }
 var btn = document.getElementById("top");
-
 function up() {
   if (document.documentElement.scrollTop > 100) {
     btn.style.display = "block";
@@ -13,8 +18,7 @@ function up() {
     btn.style.display = "none";
   }
 }
-
-window.onscroll = up;
 function topbtn() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
+window.onscroll = up;
